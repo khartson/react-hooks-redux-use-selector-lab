97d228a6c1c5cd552270f381950a096f3947df42
Kyle <khartson2017@gmail.com>
2022-09-20 10:48:56 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@ import App from "./App";
 import usersReducer from "./features/users/usersSlice";
 
 // add imports and code
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  usersReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 ReactDOM.render(
   // add imports and code
+  <Provider store={store}>
   <App />,
+  </Provider>,
   // add imports and code
   document.getElementById("root")
 );
